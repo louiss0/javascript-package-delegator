d4f42fdfb4c2d9ea400730aea2afbf00b55ac44a
Shelton Louis <louisshelton0@gmail.com>
2025-07-09 20:17:16 -0400

    fix: remove default nix file I figured out that this file doesn't belong here it belongs in a different repo!

--- default.nix
+++ default.nix
@@ -7,3 +7,5 @@
   jpd = pkgs.callPackage dist/nix/pkgs/javascript-package-delegator { };
 
 }
+# curl -O https://github.com/louiss0/javascript-package-delegator/releases/download/v1.0.0/javascript-package-delegator_1.0.0_linux_amd64.tar.gz
+# sha256sum javascript-package-delegator_1.0.0_linux_amd64.tar.gz
