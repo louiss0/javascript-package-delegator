205f929ca52cfe027ff9354218a3e288187533a9
Shelton Louis <louisshelton0@gmail.com>
2025-08-19 12:00:44 -0400

    fix(test): Add back Ginkgo test suite for javascript package delegator

---
+++ javascript_package_delegator_suite_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+)
+
+func TestJavascriptPackageDelegator(t *testing.T) {
+	RunSpecs(t, "JavascriptPackageDelegator Suite")
+}
